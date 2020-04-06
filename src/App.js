4c25eb02dc9a147a62e93ccbb895cdd63e1980d1
IFFANCOE <vbaako1@hotmail.com>
2020-04-06 23:42:47 +0700

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,79 +1,91 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
-import {firestore} from './index'
 import Menu from './components/Menu'
-import './components/Menu.css'
-function App() {
-  const [menus, setMenus] = useState([])
-    const [name,setName] =useState('')
-    const [img,setImg] =useState([])
+import { useState, useEffect } from 'react';
+import {firestore} from './index'
+import React from 'react';
+import  './components/login'
+ const App = ()=> {
 
-    useEffect( ()=>{
-      retriveData()
-    },[])
+ const [menus, setMenus] = useState([]);
+ const [name,setName] = useState(''); 
 
-    const retriveData =() =>{
-      firestore.collection("menus").onSnapshot( (snapshot) =>{
-        console.log(snapshot.docs)
+ useEffect(()=>{
+    retriveData();
+ },[])
 
-    let mymenu= snapshot.docs.map( d =>{
-          const {id,name,img} = d.data()
-          console.log(id,name,img)
-          return {id,name,img}
-        } )
-        setMenus(mymenu)
-      })
-    }
 
-    const photo = (e) => {
-      img.push(e);
-      setImg([...img]);
-  }
-    const deleteMenu = (id) =>{
-    firestore.collection("menus").doc(id+"").delete()
-    }
+ const retriveData =() =>{
+   firestore.collection("menus").onSnapshot( (snapshot) =>{
+     console.log(snapshot.docs)
 
-    const editMenu = (id) => {
-      firestore.collection("menus").doc(id+"").set({id,name,img})
-    }
+ let mymenu= snapshot.docs.map( d =>{
+       const {id,name} = d.data()
+       console.log(id,name)
+       return {id,name}
+     } )
+     setMenus(mymenu)
+   })
+ }
 
-  const renderMenu = () => {
-  
 
-    if (menus && menus.length) {
-      return menus.map((menu, index) => {
-        return (
-         <Menu key ={index} menu={menu}
-         deleteMenu={deleteMenu}
-         editMenu={editMenu}
-         photo = {photo}
-         />
-        )
-      })
-    }
-    else{ return (<li> No Menu </li>) }
+ const deleteMenu = (id) =>{
+ firestore.collection("menus").doc(id+"").delete()
+ }
+
+ const editMenu = (id) => {
+   firestore.collection("menus").doc(id+"").set({id,name})
+ }
+
+const renderMenu = () => {
+ if (menus && menus.length) {
+   return menus.map((menu, index) => {
+     return (
+      <Menu key ={index} menu={menu}
+      deleteMenu={deleteMenu}
+      editMenu={editMenu}
       
-  }
-  const addMenu = () =>{ 
-    let id = (menus.length === 0)?1:menus[menus.length-1].id +1 ;
-
-    firestore.collection("menus").doc(id+"").set( {id, name,img} )
-  }
-  
-  return (
-    <div className='inpt'>
-      <h1>Menu</h1>
-      
-    <div> <input type='text' name="name" onChange={ (e) =>{setName(e.target.value)}}/>  </div> 
-     
-      <input type='file' accept='image/*'onChange={ (e) =>{setImg(e.target.value)}}/>
-      
-      <div><button onClick={addMenu}>Summit</button> </div>
-      
-     <div className="dis">{renderMenu()}</div> 
-
-    </div>
-  );
+      />
+     )
+   })
+ }
+ else{ return (<li> No Menu </li>) }
+   
 }
 
-export default App;
+const addMenu = () =>{ 
+ let id = (menus.length === 0)?1:menus[menus.length-1].id +1 ;
+
+ firestore.collection("menus").doc(id+"").set( {id, name} )
+}
+
+return (
+ 
+ <div >
+   <login/> 
+   <div className='top'>   
+   <tr>   
+ <td>  <h1 >Menu</h1>  </td>
+   </tr>
+   <tr> 
+  <td> <div> <input type='text' name="name" onChange={ (e) =>{setName(e.target.value)}}/>  </div> </td>
+   </tr>
+  {/* <tr>
+ <td><input type='file' accept='image/*'onChange={ (e) =>{setImg(e.target.value)}}/> </td> 
+  </tr> */}
+   <tr>
+   <td><div><button onClick={addMenu}>Summit</button> </div> </td>
+   </tr>
+  
+   </div>
+  
+  <div className="dis">
+  {renderMenu()}
+  </div> 
+
+ </div>
+);
+
+ }
+
+ export default App;
+
+
